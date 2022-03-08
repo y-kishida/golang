38caf3e6a5a15f862919466e9d0ebe09fd4443ff
y-kishida <k.yoshikazu1221@gmail.com>
2022-03-08 21:47:57 +0900

    lesson operator

---
+++ section6/compare_operator.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println(1 == 1)
+
+	fmt.Println(1 == 2)
+
+	fmt.Println(8 <= 8)
+
+	fmt.Println(1 >= 8)
+
+	fmt.Println(8 < 8)
+}
---
+++ section6/logic_operator.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println(true && false == true)
+	fmt.Println(true && true == true)
+
+	fmt.Println(true || false == true)
+	fmt.Println(false || false == true)
+
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
